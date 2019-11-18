cea3727185d3ab7565c9b1f58488a835d131e1ff
julien pinchelimouroux <julien.pinchelimouroux@gmail.com>
2019-11-18 22:29:12 +0100

    Adding cobweb for spider : amount and size increase with fear

---
+++ Assets/Resources/Scripts/SpawnerMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpawnerMaterial : MonoBehaviour
+{
+    public List<GameObject> objects = new List<GameObject>();
+
+    public void Activate(float power)
+    {
+        objects.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+        float count = Mathf.Lerp(0, objects.Count, power);
+        for (int i = 0; i < count; ++i)
+        {
+            objects[i].SetActive(true);
+        }
+    }
+}
