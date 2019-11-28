2bf06b6b2addc0a4c7d296dd01ae9f9bbda6b72c
unknown <robin.millian@epitech.eu>
2019-11-28 15:31:49 +0100

    Add: Falling physics for player

---
+++ Assets/makeFall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class makeFall : MonoBehaviour
+{
+    private bool move = false;
+    public float speed = 24f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!move)
+        return ;
+        transform.position += Vector3.up * Time.deltaTime * speed;
+        speed += 0.5f;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            move = true;
+        }
+    }
+}
