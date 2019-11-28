ca8be16b38584bdd71f75c83a8af09faecdd29f7
plean <jean.plancher@epitech.eu>
2019-11-28 17:25:20 +0100

    Fracturing into multiples meshes

--- Assets/MeshTest.cs
+++ Assets/MeshTest.cs
@@ -112,7 +112,7 @@ public class MeshTest : MonoBehaviour
 	return is_destroyed;
     }
 
-    private static Mesh DestructionTriangle(Mesh obj, int i)
+    private static List<Mesh> DestructionTriangle(Mesh obj, int i)
     {
 	List<bool> is_destroyed = new List<bool>(obj.triangles.Length);
 	for (int idx = 0; idx < 100; idx += 1)
@@ -120,6 +120,8 @@ public class MeshTest : MonoBehaviour
 		is_destroyed.Add(false);
 	}
 	is_destroyed = GetDestroyedTriangles(obj, i, is_destroyed);
+
+	List<Mesh> meshes = new List<Mesh>();
 
 	Mesh new_obj = new Mesh();
 	new_obj.uv = obj.uv;
@@ -140,6 +142,7 @@ public class MeshTest : MonoBehaviour
 
 	     if (is_destroyed[idx])
 	     {
+
 		// We create and add the new point
 		Vector3 new_point = new Vector3((face[0].x + face[1].x + face[2].x) / 3,
 						(face[0].y + face[1].y + face[2].y) / 3,
@@ -147,17 +150,28 @@ public class MeshTest : MonoBehaviour
 		);
 		vertices.Add(new_point);
 
-		// We add the 3 new triangles
-		triangles.AddRange(new int[]{obj.triangles[idx],
-					     obj.triangles[idx + 1],
-					     vertices.Count - 1,
-					     vertices.Count - 1,
-					     obj.triangles[idx + 1],
-					     obj.triangles[idx + 2],
-					     obj.triangles[idx],
-					     vertices.Count - 1,
-					     obj.triangles[idx + 2]});
-		
+		// We create a new mesh
+		// TODO: This may need to be optimized in further version
+		Mesh new_mesh = new Mesh();
+		new_mesh.uv = obj.uv;
+		new_mesh.uv2 = obj.uv2;
+		new_mesh.colors = obj.colors;
+		new_mesh.SetVertices(vertices);
+		List<int> new_mesh_triangles = new List<int>();
+
+		// We add the 3 new triangles to the new mesh
+		new_mesh_triangles.AddRange(new int[]{obj.triangles[idx],
+						      obj.triangles[idx + 1],
+					     	      vertices.Count - 1,
+					     	      vertices.Count - 1,
+					     	      obj.triangles[idx + 1],
+					     	      obj.triangles[idx + 2],
+					     	      obj.triangles[idx],
+					     	      vertices.Count - 1,
+					     	      obj.triangles[idx + 2]});
+
+		new_mesh.triangles = new_mesh_triangles.ToArray();
+		meshes.Add(new_mesh);
 	     }
 	     else
 	     {
@@ -171,7 +185,8 @@ public class MeshTest : MonoBehaviour
 	new_obj.triangles = triangles.ToArray();
 
 
-	return new_obj;
+	meshes.Add(new_obj);
+	return meshes;
     }
 
     /// <summary>
@@ -180,8 +195,8 @@ public class MeshTest : MonoBehaviour
     /// <param name="obj">Object to destroy.</param>
     /// <param name="anchorPoint">Impact point.</param>
     /// <param name="normalDirection">Direction of the impact.</param>
-    /// <returns>A new mesh object</returns>
-    private static Mesh DestructionMesh(Mesh obj, Vector3 anchorPoint, Vector3 normalDirection)
+    /// <returns>A list of new mesh object</returns>
+    private static List<Mesh> DestructionMesh(Mesh obj, Vector3 anchorPoint, Vector3 normalDirection)
     {
 	 for (int i = 0; i < obj.triangles.Length; i += 3) {
 	     bool result = Intersect(obj.vertices[obj.triangles[i]],
@@ -192,12 +207,14 @@ public class MeshTest : MonoBehaviour
 	     	return DestructionTriangle(obj, i);
 	     }
 	 }
-	 return obj;
+	 List<Mesh> ret = new List<Mesh>();
+	 ret.Add(obj);
+	 return ret;
     }
 
     /// This one is for debug
     /// The impact point is random
-    private static Mesh DestructionMesh(Mesh obj)
+    private static List<Mesh> DestructionMesh(Mesh obj)
     {
 	 return DestructionTriangle(obj, UnityEngine.Random.Range(0, obj.triangles.Length));
     }
